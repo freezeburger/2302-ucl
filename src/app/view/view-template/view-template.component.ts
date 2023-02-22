9df1502f78b4947df2cf9bf77db8b9fa06cd8ad6
orsys <orsys@orsys.fr>
2023-02-22 14:15:50 +0100

    View Components

---
+++ src/app/view/view-template/view-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-template',
+  templateUrl: './view-template.component.html',
+  styleUrls: ['./view-template.component.scss']
+})
+export class ViewTemplateComponent {
+
+}
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 
 import { ViewRoutingModule } from './view-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ViewHomeComponent } from './view-home/view-home.component';
+import { ViewMessageComponent } from './view-message/view-message.component';
+import { ViewInfoComponent } from './view-info/view-info.component';
+import { ViewTemplateComponent } from './view-template/view-template.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewHomeComponent,
+    ViewMessageComponent,
+    ViewInfoComponent,
+    ViewTemplateComponent
+  ],
   imports: [
     ViewRoutingModule,
     SharedModule
+  ],
+  exports: [
+    ViewHomeComponent,
+    ViewMessageComponent,
+    ViewInfoComponent
   ]
 })
 export class ViewModule { }
