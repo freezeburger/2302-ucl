cb149d28684279e366823a2f4beaad7afa85aebd
orsys <orsys@orsys.fr>
2023-02-22 13:48:32 +0100

    Structure

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ViewRoutingModule } from './view-routing.module';
 import { SharedModule } from '../shared/shared.module';
@@ -8,7 +7,6 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     ViewRoutingModule,
     SharedModule
   ]
