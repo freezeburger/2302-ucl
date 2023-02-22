b2d8ccbcbc003e975018a86535629f542722b49d
orsys <orsys@orsys.fr>
2023-02-22 16:18:50 +0100

    Navigation

--- src/app/view/view-routing.module.ts
+++ src/app/view/view-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewHomeComponent } from './view-home/view-home.component';
+import { ViewInfoComponent } from './view-info/view-info.component';
+import { ViewMessageComponent } from './view-message/view-message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'info',
+    component:ViewInfoComponent
+  },
+  {
+    path:'message',
+    component:ViewMessageComponent
+  },
+  {
+    path:'',
+    component:ViewHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
