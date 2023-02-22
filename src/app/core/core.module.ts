98096b2e10aa3284986ca257571cc337cda174ea
orsys <orsys@orsys.fr>
2023-02-22 14:14:21 +0100

    Update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { Inject, NgModule } from '@angular/core';
+import { inject, Inject, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
 import { MessageService } from './services/message.service';
@@ -16,9 +16,11 @@ import { BusEventService } from './services/bus-event.service';
   ]
 })
 export class CoreModule { 
+  // auth = inject(AuthService)
   constructor(
-    @Inject(AuthService) auth:AuthService
+    /* @Inject(AuthService) auth:AuthService */
+    private auth:AuthService
   ){
-    console.warn(auth)
+    console.warn(this.auth)
   }
 }
