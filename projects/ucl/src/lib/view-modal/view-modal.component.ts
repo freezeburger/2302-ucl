b1b62cf5ad3052757647e4a21620ef41b8152f2e
orsys <orsys@orsys.fr>
2023-02-23 13:43:10 +0100

    Sub Component Simplification

--- projects/ucl/src/lib/view-modal/view-modal.component.ts
+++ projects/ucl/src/lib/view-modal/view-modal.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ucl-view-modal',
-  templateUrl: './view-modal.component.html',
-  styleUrls: ['./view-modal.component.css']
+  template: `<ng-content></ng-content>`
 })
-export class ViewModalComponent {
-
-}
+export class ViewModalComponent {}
