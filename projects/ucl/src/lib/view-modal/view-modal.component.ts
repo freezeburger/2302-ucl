457911b632a739cd109365859538a783ced34d95
orsys <orsys@orsys.fr>
2023-02-23 13:58:24 +0100

    Sub Component Control

--- projects/ucl/src/lib/view-modal/view-modal.component.ts
+++ projects/ucl/src/lib/view-modal/view-modal.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { ViewTemplateComponent } from '../view-template/view-template.component';
 
 @Component({
   selector: 'ucl-view-modal',
   template: `<ng-content></ng-content>`
 })
-export class ViewModalComponent {}
+export class ViewModalComponent {
+  constructor(
+    @Optional() private uclTemplate:ViewTemplateComponent
+  ){
+    if(!uclTemplate) console.log(`%c 
+"ViewModalComponent" must be used inside ViewTemplateComponent :
+        <ucl-view-template> 
+          <ucl-view-modal> 
+          </ucl-view-modal> 
+        </ucl-view-template> 
+    `, 'color:hotPink')
+  }
+}
