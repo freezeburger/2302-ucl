07cde80252c24692fad07cd1e37ef0364e4cb9aa
orsys <orsys@orsys.fr>
2023-02-24 13:48:22 +0100

    Basic Auth Implements

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FuncUser } from '../interfaces/functional/user';
+
+const AUTH_API = 'http://localhost:5050/auth';
+const AUTH_LOGIN = AUTH_API + '/login';
+const AUTH_REGISTER = AUTH_API + '/register';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  login(credentials:FuncUser){
+    // Crendentials Sanitization
+    this.http.post(AUTH_LOGIN, credentials).subscribe(console.log)
+  }
+
+  register(credentials:FuncUser){
+    // Crendentials Sanitization
+    this.http.post(AUTH_LOGIN, credentials).subscribe(console.log)
+  }
+
 }
