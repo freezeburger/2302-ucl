7fe19aed680b6535f4e72b8bc0f6ba2335026064
orsys <orsys@orsys.fr>
2023-02-24 10:20:32 +0100

    Promise Theory

--- src/promise-to-observable.ts
+++ src/promise-to-observable.ts
@@ -4,7 +4,7 @@
  */
 
 // Etant producer fonction calculant input*10 de facon asyncrhone
-const producer = (val: number, cb: any) => {
+/* const producer = (val: number, cb: any) => {
     const delay = 3000 * Math.random();
     let result: any;
     setTimeout(
@@ -22,5 +22,46 @@ const consumer = (data: any) => {
     return data;
 }
 
-producer(10 , consumer);
+producer( 10 , consumer );
+ */
 
+// Programmation par promise
+/* 
+const producer = (val: number): Promise<number> => {
+    const delay = 3000 * Math.random();
+    const promise = new Promise<number>((resolve, reject) => {
+        let result: number;
+        setTimeout(
+            () => {
+                result = val * 10;
+                resolve(result)
+            }
+            , delay
+        );
+    });
+    return promise;
+}
+
+const consumer = (data: any) => {
+    console.log({ data });
+    return data;
+}
+
+producer( 10 )
+    .then( consumer )
+    .then( data => data + 1)
+    .then( data => producer( data ) )
+    .then( consumer ) 
+
+const main = async () => {
+    const res = await producer( 10 );
+    console.log('Done')
+    return res;
+}
+main();
+console.log('Main Done ?')
+ */
+
+/**
+ * Observable
+ */
